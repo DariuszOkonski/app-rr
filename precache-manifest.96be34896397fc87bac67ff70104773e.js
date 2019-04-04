969348f7b6de7482359a08abcc2e7b9d2af5301c
Dariusz Okonski <darek200180@gmail.com>
2019-04-04 23:35:10 +0100

    Updates

---
+++ precache-manifest.96be34896397fc87bac67ff70104773e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f23e35a2fb94499a5fc8585e2212e7f3",
+    "url": "/app-rr/./static/media/header5.f23e35a2.jpg"
+  },
+  {
+    "revision": "0b44f6ea1d98df46e12810ddabe6c917",
+    "url": "/app-rr/./static/media/header4.0b44f6ea.jpg"
+  },
+  {
+    "revision": "312c13818e52c18cc3ed244f0b723f0f",
+    "url": "/app-rr/./static/media/header3.312c1381.jpg"
+  },
+  {
+    "revision": "23025d81c2a9f94221dc89713b6902a0",
+    "url": "/app-rr/./static/media/header2.23025d81.jpg"
+  },
+  {
+    "revision": "7a49c3f48731e1e53b4e60717f3a2660",
+    "url": "/app-rr/./static/media/header1.7a49c3f4.jpg"
+  },
+  {
+    "revision": "a6d34108e9c3d9400fde",
+    "url": "/app-rr/./static/js/runtime~main.a6d34108.js"
+  },
+  {
+    "revision": "0930d0627753bbc8ea2f",
+    "url": "/app-rr/./static/js/main.0930d062.chunk.js"
+  },
+  {
+    "revision": "852bb30b40c8533a81d0",
+    "url": "/app-rr/./static/js/2.852bb30b.chunk.js"
+  },
+  {
+    "revision": "0930d0627753bbc8ea2f",
+    "url": "/app-rr/./static/css/main.a233c931.chunk.css"
+  },
+  {
+    "revision": "12becb4071222a073e37f8cbbdcd7fb5",
+    "url": "/app-rr/./index.html"
+  }
+];
